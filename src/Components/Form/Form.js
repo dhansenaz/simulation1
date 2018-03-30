a70886d7bb3739f59451f379282989efedd888ec
dhansenaz <dhansen@allthingsdot.com>
2018-03-30 14:58:53 -0700

    second commit

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import './form.css'
 
 class Form extends Component{
     constructor(){
@@ -8,7 +9,22 @@ class Form extends Component{
 render(){
     return(
         <div>
-            Form
+            <form>
+                <div>
+                    <input className="one"></input>
+                   
+                        <div>
+                        <input className="two"></input>
+                        </div>
+                    
+                    <input className="three"></input>
+                        <div>
+                            <button className="cancel">Cancel</button>
+                            <button className="addinventory">Add to Inventory</button>
+                        </div>
+                </div>
+                
+            </form>
         </div>
     )
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import './logo.png';
 
 
 export default function Header() {
 
     return (
-        <div className="header">
-            <div>
-                <img className="logo" src="logo.png"></img>
+        <div>
+            <div className="header">
+                <img className="logo" src="http://www.freeiconspng.com/uploads/spotify-icon-1.png"></img>
+                <h1>SHELFIE</h1>
             </div>
-            Header
+           
         </div>
     )
 
